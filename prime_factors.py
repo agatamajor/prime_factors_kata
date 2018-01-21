86471035f8ed3ec2c9e49449d1b287e5c892db4f
agata <agata.a.major@gmail.com>
2018-01-21 21:32:04 +0100

    first assert

--- prime_factors.py
+++ prime_factors.py
@@ -1,9 +1,16 @@
 import unittest
 
 def get_prime_factors(number):
-    return []
-
+    list = []
+    if number == 2:
+        list.append(2)
+    if number == 3:
+        list.append(3)
+    return list
 
 assert get_prime_factors(1) == []
 assert get_prime_factors(2) == [2]
+assert get_prime_factors(3) == [3]
+
+
 
